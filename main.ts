ea3c16ca5a5141d48992a2dcdb4d766a93724fb7
liuliuhuang <164977841+liuliuhuang@users.noreply.github.com>
2024-03-28 12:04:19 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,121 @@
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    mySprite.sayText("go, go, go")
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    mySprite.sayText("stop")
+})
+function Joe () {
+    mySprite = sprites.create(img`
+        . . . . . . f f f f . . . . . . 
+        . . . . f f f 2 2 f f f . . . . 
+        . . . f f f 2 2 2 2 f f f . . . 
+        . . f f f e e e e e e f f f . . 
+        . . f f e 2 2 2 2 2 2 e e f . . 
+        . . f e 2 f f f f f f 2 e f . . 
+        . . f f f f e e e e f f f f . . 
+        . f f e f b f 4 4 f b f e f f . 
+        . f e e 4 1 f d d f 1 4 e e f . 
+        . . f e e d d d d d d e e f . . 
+        . . . f e e 4 4 4 4 e e f . . . 
+        . . e 4 f 2 2 2 2 2 2 f 4 e . . 
+        . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+        . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+        . . . . . f f f f f f . . . . . 
+        . . . . . f f . . f f . . . . . 
+        `, SpriteKind.Player)
+    controller.moveSprite(mySprite)
+    mySprite.setPosition(130, 96)
+    info.setLife(3)
+    mySprite.setBounceOnWall(true)
+}
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Player, function (sprite, otherSprite) {
+	
+})
+function Car () {
+    carEnermy = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . 2 2 2 2 2 2 . . . . 
+        . . . . . 2 2 4 4 2 2 2 2 . . . 
+        . . . . . c 4 2 2 2 2 2 c . . . 
+        . . . . 2 c 4 2 2 2 2 2 c 2 . . 
+        . . . e 2 c 4 2 2 2 2 2 c 2 e . 
+        . . . f 2 c 4 2 2 2 2 2 c 2 f . 
+        . . . f e c 2 2 2 2 2 2 c e f . 
+        . . . f 2 c 2 b b b b 2 c 2 f . 
+        . . . e 2 2 b c c c c b 2 2 e . 
+        . . . e e b c c c c c c b e e . 
+        . . . f e 4 4 4 4 4 4 4 4 e f . 
+        . . . f e d 2 2 2 2 2 2 d e f . 
+        . . . . 2 d d 2 2 2 2 d d 2 f . 
+        . . . . f 2 d 2 2 2 2 d 2 f . . 
+        . . . . . e 2 2 2 2 2 2 e . . . 
+        `, SpriteKind.Enemy)
+    carEnermy.setVelocity(0, 50)
+}
+function chiken () {
+    myChiken = sprites.create(img`
+        . . . b 5 b . . . . . . . . . . 
+        . . . . b 5 b . . . . . . . . . 
+        . . . . b b b b b b . . . . . . 
+        . . . b 5 5 5 5 5 b b . . . . . 
+        . . c 4 d 5 f 1 d 5 b b . . . . 
+        b 4 4 4 d d f f 1 5 5 b . . . . 
+        . b 4 4 4 4 b f d 5 5 b . . . . 
+        . . b 4 4 4 4 5 5 5 5 d b . . . 
+        . . b 5 5 5 5 5 5 5 d d d b b . 
+        . b 5 5 5 5 5 5 5 b b b d d d b 
+        . b 5 5 5 5 5 5 c d 5 5 b d d c 
+        . b 5 5 5 5 5 5 d c d 5 d b b c 
+        . b 5 5 5 5 5 5 d d c b 5 5 b c 
+        . b d 5 5 5 5 5 d d d c c c b b 
+        . . b b 5 5 5 d d d c c . . . . 
+        . . . b b c c c c c . . . . . . 
+        `, SpriteKind.Food)
+    myChiken2 = sprites.create(img`
+        . . . b 5 b . . . . . . . . . . 
+        . . . . b 5 b . . . . . . . . . 
+        . . . . b b b b b b . . . . . . 
+        . . . b 5 5 5 5 5 b b . . . . . 
+        . . c 4 d 5 f 1 d 5 b b . . . . 
+        b 4 4 4 d d f f 1 5 5 b . . . . 
+        . b 4 4 4 4 b f d 5 5 b . . . . 
+        . . b 4 4 4 4 5 5 5 5 d b . . . 
+        . . b 5 5 5 5 5 5 5 d d d b b . 
+        . b 5 5 5 5 5 5 5 b b b d d d b 
+        . b 5 5 5 5 5 5 c d 5 5 b d d c 
+        . b 5 5 5 5 5 5 d c d 5 d b b c 
+        . b 5 5 5 5 5 5 d d c b 5 5 b c 
+        . b d 5 5 5 5 5 d d d c c c b b 
+        . . b b 5 5 5 d d d c c . . . . 
+        . . . b b c c c c c . . . . . . 
+        `, SpriteKind.Food)
+    myChiken3 = sprites.create(img`
+        . . . b 5 b . . . . . . . . . . 
+        . . . . b 5 b . . . . . . . . . 
+        . . . . b b b b b b . . . . . . 
+        . . . b 5 5 5 5 5 b b . . . . . 
+        . . c 4 d 5 f 1 d 5 b b . . . . 
+        b 4 4 4 d d f f 1 5 5 b . . . . 
+        . b 4 4 4 4 b f d 5 5 b . . . . 
+        . . b 4 4 4 4 5 5 5 5 d b . . . 
+        . . b 5 5 5 5 5 5 5 d d d b b . 
+        . b 5 5 5 5 5 5 5 b b b d d d b 
+        . b 5 5 5 5 5 5 c d 5 5 b d d c 
+        . b 5 5 5 5 5 5 d c d 5 d b b c 
+        . b 5 5 5 5 5 5 d d c b 5 5 b c 
+        . b d 5 5 5 5 5 d d d c c c b b 
+        . . b b 5 5 5 d d d c c . . . . 
+        . . . b b c c c c c . . . . . . 
+        `, SpriteKind.Food)
+    myChiken.setPosition(137, 96)
+    myChiken2.setPosition(144, 96)
+    myChiken3.setPosition(151, 96)
+}
+let myChiken3: Sprite = null
+let myChiken2: Sprite = null
+let myChiken: Sprite = null
+let carEnermy: Sprite = null
+let mySprite: Sprite = null
 scene.setBackgroundImage(img`
     ddddddddddddddddddddddddddddddddddbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb111bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbddddddddddddddddddddddddddddddddddd
     ddddddddddddddddddddddddddddddddddbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb111bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbddddddddddddddddddddddddddddddddddd
@@ -120,3 +238,6 @@ scene.setBackgroundImage(img`
     ddddddddddddddddddddddddddddddddddbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbddddddddddddddddddddddddddddddddddd
     ddddddddddddddddddddddddddddddddddbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbddddddddddddddddddddddddddddddddddd
     `)
+Car()
+Joe()
+chiken()
